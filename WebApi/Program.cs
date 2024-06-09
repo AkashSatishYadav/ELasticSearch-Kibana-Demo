71d5c30cd2781203fab239309100a378df19541e
Akash Yadav <akashy211@gmail.com>
2024-06-09 16:21:43 +0530

    Add project files.

---
+++ WebApi/Program.cs
@@ -0,0 +1,32 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders();
+var log = new LoggerConfiguration().WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfig:Uri"]))
+{
+    AutoRegisterTemplate = true,
+}).CreateLogger();
+builder.Logging.AddSerilog(log);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
